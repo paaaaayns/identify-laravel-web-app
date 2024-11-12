0d1de097571cf3587a143d0674a59db8be60ae89
paaaaayns <ejpines21@gmail.com>
2024-11-12 14:05:45 +0800

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RegisterDoctorController;
+use App\Http\Controllers\RegisterOpdController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
+// Login
+Route::get('/login', [SessionController::class, 'create'])->name('login');
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
+
+Route::get('/dashboard', [DashboardController::class, 'show'])->middleware('auth')->name('dashboard');
+
+
+// Register OPD
+Route::get('/register/opd', [RegisterOpdController::class, 'create'])->middleware('auth');
+Route::post('/register/opd', [RegisterOpdController::class, 'store'])->middleware('auth');
+
+// Register Doctor
+Route::get('/register/doctor', [RegisterDoctorController::class, 'create'])->middleware('auth');
+Route::post('/register/doctor', [RegisterDoctorController::class, 'store'])->middleware('auth');
