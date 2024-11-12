a64ba5fbbbc446721d94ab208491ca3fc003ecf4
paaaaayns <ejpines21@gmail.com>
2024-11-12 13:52:26 +0800

    feat: add login auth

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(){
+        // dd('hello');
+        return view('auth.login');
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(){
+        // dd('hello');
+
+        // validate
+        $validatedData = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        // dd($validatedData);
+
+        // attempt to log in 
+        if (!Auth::attempt($validatedData)) {
+            throw ValidationException::withMessages([
+                'email' => "Credentials doesn't match"
+            ]);
+        };
+
+        // regenerate session token
+        request()->session()->regenerate();
+        
+         // get the authenticated user
+        $user = Auth::user();
+        // dd($user->type);
+
+         return redirect('/dashboard');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(){
+        // dd('logout');
+
+        Auth::logout();
+
+        return redirect('/login');
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="h-screen flex flex-row p-0 m-0">
+    <!-- Left Division -->
+    <div class="flex flex-grow min-w-1/2 max-w-1/2 items-center justify-center bg-background-dark">
+        <div class="text-center">
+            <h1 class="text-white text-2xl">Left Side Content</h1>
+        </div>
+    </div>
+
+    <!-- Right Division -->
+    <div class="flex flex-grow min-w-1/2 max-w-1/2 items-center justify-center bg-background-light">
+        <div class="text-center">
+            <form method="POST" action="/login">
+                @csrf
+
+                <x-forms.error name="test" />
+
+                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12">
+                    <x-forms.field>
+                        <x-forms.label>Email</x-forms.label>
+
+                        <x-forms.input type="text" name="email" id="email" :value="old('email')" autocomplete="off" />
+                        <x-forms.error name="email" />
+                    </x-forms.field>
+
+                    <x-forms.field>
+                        <x-forms.label>Password</x-forms.label>
+
+                        <x-forms.input type="password" name="password" id="password" autocomplete="off" />
+                        <x-forms.error name="password" />
+                    </x-forms.field>
+                </div>
+
+                <div class="mt-6 flex items-center justify-center gap-x-6">
+                    <x-forms.primary-button type="submit">Login</x-button>
+                </div>
+
+                <p class="mt-6">
+                    Not registered yet? <a href="" class="text-primary">Register Now</a>
+                </p>
+
+                <p class="mt-6">
+                    <a href="" class="text-primary">Forget Password?</a>
+                </p>
+            </form>
+
+        </div>
+    </div>
+</body>
+
+</html>
