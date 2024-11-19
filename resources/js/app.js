123f6d1aa849d132a1dbc37016ab5e5cb952cee5
paaaaayns <ejpines21@gmail.com>
2024-11-19 18:16:08 +0800

    feat: add rappasoft/livewire-tables

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,3 @@
 import './bootstrap';
-import 'simple-datatables/dist/style.css';
 
-
-import $ from 'jquery';
-import 'datatables.net';
-
-$(document).ready(function() {
-    if (document.getElementById("default-table") && typeof simpleDatatables.DataTable !== 'undefined') {
-        const dataTable = new simpleDatatables.DataTable("#default-table", {
-            searchable: true,
-            sortable: false
-        });
-    }
-});
+import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables-all.js';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,41 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-        './node_modules/flowbite/**/*.js',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+        "./vendor/rappasoft/laravel-livewire-tables/resources/views/*.blade.php",
+        "./vendor/rappasoft/laravel-livewire-tables/resources/views/**/*.blade.php",
+        "./app/Livewire/*.php",
+        "./app/Livewire/**/*.php",
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
             colors: {
                 primary: {
-                    DEFAULT: '#1F555F',   // Main primary color
+                    DEFAULT: "#1F555F", // Main primary color
                 },
                 secondary: {
-                    DEFAULT: '#DDE9F1',   // Main primary color
+                    DEFAULT: "#DDE9F1", // Main primary color
                 },
                 background: {
-                    light: '#DDE9F1',  // Light background color
-                    dark: '#1F555F',    // Dark background color
+                    light: "#DDE9F1", // Light background color
+                    dark: "#1F555F", // Dark background color
                 },
                 button: {
                     primary: {
-                      DEFAULT: '#1F555F',       // Primary button color
-                      hover: '#276873',         // Lighter hover color
-                      active: '#173D45',        // Darker active color
-                      disabled: '#A0B5BA'       // Muted disabled color
+                        DEFAULT: "#1F555F", // Primary button color
+                        hover: "#276873", // Lighter hover color
+                        active: "#173D45", // Darker active color
+                        disabled: "#A0B5BA", // Muted disabled color
                     },
                 },
             },
@@ -39,12 +43,8 @@ export default {
     },
     plugins: [
         // require('@tailwindcss/forms'),
-        require('flowbite/plugin')({
+        require("flowbite/plugin")({
             datatables: true,
         }),
     ],
-
-    
-    
 };
-
