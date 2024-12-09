710df19c37d2aa5420b431ffa00adadc49411fc4
paaaaayns <ejpines21@gmail.com>
2024-12-09 18:07:14 +0800

    feat: add dashboard view for OPD

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Doctor;
 use App\Models\Opd;
 use App\Models\Patient;
+use App\Models\PatientQueue;
 use App\Models\PreRegisteredPatient;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -23,16 +24,16 @@ class DashboardController extends Controller
         switch ($user->type) {
             case 'ADMIN':
                 $preRegPatientsCount = PreRegisteredPatient::count();
-                $recentPreRegPatientsCount = PreRegisteredPatient::where('created_at', '>=', Carbon::now()->subDay())->count();
+                $recentPreRegPatientsCount = PreRegisteredPatient::where('created_at', '>=', Carbon::now()->subHours(3))->count();
 
                 $patientsCount = Patient::count();
-                $recentPatientsCount = Patient::where('created_at', '>=', Carbon::now()->subDay())->count();
+                $recentPatientsCount = Patient::where('created_at', '>=', Carbon::now()->subHours(3))->count();
 
                 $opdsCount = Opd::count();
-                $recentOpdsCount = Opd::where('created_at', '>=', Carbon::now()->subDay())->count();
+                $recentOpdsCount = Opd::where('created_at', '>=', Carbon::now()->subHours(3))->count();
 
                 $doctorsCount = Doctor::count();
-                $recentDoctorsCount  = Doctor::where('created_at', '>=', Carbon::now()->subDay())->count();
+                $recentDoctorsCount  = Doctor::where('created_at', '>=', Carbon::now()->subHours(3))->count();
 
                 return view('auth.dashboard.admin', [
                     'preRegPatientsCount' => $preRegPatientsCount,
@@ -49,7 +50,21 @@ class DashboardController extends Controller
                 ]);
 
             case 'OPD':
-                return view('auth.dashboard.opd');
+                $preRegPatientsCount = PreRegisteredPatient::count();
+                $recentPreRegPatientsCount = PreRegisteredPatient::where('created_at', '>=', Carbon::now()->subHours(3))->count();
+
+                $queuedPatientsCount = PatientQueue::where('queue_status', 'waiting')
+                    ->whereDate('queued_at', Carbon::today())
+                    ->count();
+                $recentQueuedPatientsCount = 0;
+
+                return view('auth.dashboard.opd', [
+                    'preRegPatientsCount' => $preRegPatientsCount,
+                    'recentPreRegPatientsCount' => $recentPreRegPatientsCount,
+
+                    'queuedPatientsCount' => $queuedPatientsCount,
+                    'recentQueuedPatientsCount' => $recentQueuedPatientsCount,
+                ]);
             case 'DOCTOR':
                 return view('auth.dashboard.doctor');
             case 'PATIENT':
