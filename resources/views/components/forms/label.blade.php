c83747970858a17fdf22cd3589c537b20b82fb9b
paaaaayns <ejpines21@gmail.com>
2024-12-10 14:33:14 +0800

    feat: add required prop to label component for indicating mandatory fields

--- resources/views/components/forms/label.blade.php
+++ resources/views/components/forms/label.blade.php
@@ -1,6 +1,10 @@
+@props([
+    'required' => false
+])
+
 <label
     {{ $attributes->merge([
         'class' => 'block text-sm/6 font-medium text-gray-900'
         ]) }}>
-    {{ $slot }}
+    {{ $slot }} @if ($required) <span class="text-red-500">*</span> @endif
 </label>
