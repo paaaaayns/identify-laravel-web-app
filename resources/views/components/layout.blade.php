b903923cbc94f789aae8b7d1d510e4d6c62f65c7
paaaaayns <ejpines21@gmail.com>
2024-12-12 08:45:38 +0800

    feat: enhance user profile view with biometric information and capture functionality

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -851,7 +851,6 @@
       <button
          type="button"
          onclick="hideToast('toast-success')"
-         data-dismiss-target="#toast-success"
          aria-label="Close"
          class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">
          <span class="sr-only">Close</span>
@@ -873,7 +872,6 @@
       <button
          type="button"
          onclick="hideToast('toast-error')"
-         data-dismiss-target="#toast-error"
          aria-label="Close"
          class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">
          <span class="sr-only">Close</span>
