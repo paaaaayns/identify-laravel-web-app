8f2713c4c04e0a231418bf4946e5f6c094b76ae6
paaaaayns <ejpines21@gmail.com>
2024-12-19 17:43:46 +0800

    feat: add vitals information form and doctor selection component to queue view

---
+++ resources/views/auth/queue/tab-doctor.blade.php
@@ -0,0 +1,8 @@
+<!-- Doctor Selection -->
+<div class="bg-white shadow rounded-lg p-6">
+    <h3 class="text-xl font-semibold text-gray-800">Doctor Selection</h3>
+
+    <div class="mt-6">
+        <livewire:doctor-selection-table />
+    </div>
+</div>
