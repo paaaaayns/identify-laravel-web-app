036eecae725457f873d6dae0b4b81d37d596a4ed
paaaaayns <ejpines21@gmail.com>
2024-12-20 14:51:26 +0800

    feat: add email verification routes and update patient/user routes for consistency

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DataTableController;
 use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\EmailVerificationController;
 use App\Http\Controllers\OpdController;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\PatientQueueController;
@@ -36,6 +37,7 @@ Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
 
 Route::post('/verify-password', [UserController::class, 'verifyPassword']);
+Route::get('/verify-email', [UserController::class, 'verifyEmail']);
 
 
 Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard')
@@ -45,7 +47,6 @@ Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard'
 // Pre-Registration Public Routes
 Route::get('/pre-register', [PreRegistrationController::class, 'create'])->name('pre-reg.create');
 Route::post('/pre-register/store', [PreRegistrationController::class, 'store'])->name('pre-reg.store'); // TOD0: Implement this
-Route::get('/pre-register/instructions', [PreRegistrationController::class, 'instructions'])->name('pre-reg.instructions'); // TOD0: Implement this
 
 
 
@@ -59,18 +60,29 @@ Route::get('/users/pre-reg', [PreRegisteredPatientController::class, 'index'])->
 Route::get('/users/pre-reg/create', [PreRegisteredPatientController::class, 'create'])->name('users.pre-reg.create');
 Route::post('/users/pre-reg/validate-store-request', [PreRegisteredPatientController::class, 'validateStoreRequest']);
 Route::post('/users/pre-reg/store', [PreRegisteredPatientController::class, 'store'])->name('users.pre-reg.store');
-Route::get('/users/pre-reg/{user_id}', [PreRegisteredPatientController::class, 'show'])->name('users.pre-reg.show');
+Route::get('/users/pre-reg/{ulid}', [PreRegisteredPatientController::class, 'show'])->name('users.pre-reg.show');
 Route::delete('/users/pre-reg/{user_id}', [PreRegisteredPatientController::class, 'destroy'])->name('users.pre-reg.destroy');
 // Route::post('/users/pre-reg/send-confirmation-email', [PreRegisteredPatientController::class, 'sendConfirmationEmail'])->name('users.pre-reg.send-confirmation-email');
 
+Route::get('/email/verify', [EmailVerificationController::class, 'notice'])
+    ->middleware('auth')
+    ->name('verification.notice');
 
-
+Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])
+    ->middleware(['auth', 'signed'])
+    ->name('verification.verify');
+    
+Route::post('/email/verification-notification', [EmailVerificationController::class, 'send'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
 
 
 // Patient
 Route::get('/users/patient', [PatientController::class, 'index'])->name('users.patient.index');
-Route::get('/users/patient/create', [PatientController::class, 'create'])->name('users.patient.create');
-Route::get('/users/patient/{user_id}', [PatientController::class, 'show'])->name('users.patient.show');
+Route::post('/users/patient/queue', [PatientController::class, 'queue'])->name('users.patient.queue'); // TODO: Implement this
+Route::post('/users/patient/validate-store-request', [PatientController::class, 'validateStoreRequest']);
+Route::post('/users/patient/store', [PatientController::class, 'store'])->name('users.patient.store');
+Route::get('/users/patient/{ulid}', [PatientController::class, 'show'])->name('users.patient.show');
 Route::delete('/users/patient/{user_id}', [PatientController::class, 'destroy'])->name('users.patient.destroy');
 
 
@@ -96,14 +108,18 @@ Route::delete('/users/doctor/{user_id}', [DoctorController::class, 'destroy'])->
 
 // Queued Patient
 Route::get('/queue', [PatientQueueController::class, 'index'])->name('queue.index');
+Route::get('/queue/create', [PatientQueueController::class, 'create'])->name('queue.create');
+Route::post('/queue/store', [PatientQueueController::class, 'store'])->name('queue.store');
+Route::get('/queue/{ulid}', [PatientQueueController::class, 'show'])->name('queue.show');
 
 // Test email
 Route::get('/test-email', function () {
 
     $data = [
         'subject' => 'Test Email',
-        'message' => 'This is a test email.'];
-    
+        'message' => 'This is a test email.'
+    ];
+
     Mail::to('ejpines21@gmail.com')->send(new TestMail($data));
 
     return 'Test email sent successfully!';
