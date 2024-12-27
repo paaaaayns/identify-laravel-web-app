a323e4eb4e88c0082a3a66f4fad88b6de5eff4d8
paaaaayns <ejpines21@gmail.com>
2024-12-27 13:15:13 +0800

    feat: pass queue data to doctor selection table component

--- resources/views/auth/queue/tab-doctor.blade.php
+++ resources/views/auth/queue/tab-doctor.blade.php
@@ -3,6 +3,6 @@
     <h3 class="text-xl font-semibold text-gray-800">Doctor Selection</h3>
 
     <div class="mt-6">
-        <livewire:doctor-selection-table />
+        <livewire:doctor-selection-table :queue="$queue" />
     </div>
 </div>
