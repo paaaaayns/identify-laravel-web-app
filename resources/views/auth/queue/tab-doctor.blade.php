d43ac7ce850cbed06f2fb2d4bb22e2561f6fc787
paaaaayns <ejpines21@gmail.com>
2025-01-02 17:12:40 +0800

    feat: implement doctor selection and update functionality in queue management

--- resources/views/auth/queue/tab-doctor.blade.php
+++ resources/views/auth/queue/tab-doctor.blade.php
@@ -3,6 +3,28 @@
     <h3 class="text-xl font-semibold text-gray-800">Doctor Selection</h3>
 
     <div class="mt-6">
+
+        @if ($queue->doctor_id == '')
         <livewire:doctor-selection-table :queue="$queue" />
+
+        @else
+        <x-forms.field-container class="sm:col-span-4">
+            <x-forms.label
+                for="doctor">
+                Doctor
+            </x-forms.label>
+            @if (true)
+            <x-forms.input
+                type="text"
+                id="doctor"
+                name="doctor"
+                :value="'Dr. ' . $doctor->first_name . ' ' . $doctor->middle_name . ' ' . $doctor->last_name"
+                autocomplete="off"
+                disabled />
+            <x-forms.error name="doctor" />
+            @endif
+        </x-forms.field-container>
+        @endif
     </div>
+
 </div>
--- resources/views/components/livewire/datatables/doctor-selection-column.blade.php
+++ resources/views/components/livewire/datatables/doctor-selection-column.blade.php
@@ -1,10 +1,58 @@
 <div class="inline-flex items-center mx-auto space-x-3">
-    <form action="{{ route('queue.update', ['ulid' => $queue->ulid]) }}" method="POST">
+
+    <form
+        method="POST"
+        id="DoctorSelectionForm"
+        action="{{ route('queue.update', ['ulid' => $queue->ulid]) }}">
         @csrf
         @method('PUT')
-        <input type="hidden" name="doctor_id" value="{{ $doctor_id }}">
-        <button type="submit" class="text-primary">
+        <button
+            type="button"
+            onclick="updateDoctor('{{ $doctor_id }}')"
+            class="text-primary">
             Select
         </button>
+
+        @if (true)
+        <button
+            type="submit"
+            class="text-primary">
+            Override
+        </button>
+        @endif
     </form>
 </div>
+<script>
+    async function updateDoctor(doctor_id) {
+        const form = document.getElementById('DoctorSelectionForm');
+        const formAction = form.action;
+        const formData = new FormData(form);
+        // Append the doctor_id to the form data
+        formData.append('doctor_id', doctor_id);
+
+        try {
+            const response = await fetch(formAction, {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                    'Accept': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                showToast('toast-success', result.message);
+                console.log(response.status, result.message, result.queue);
+                location.reload();
+            } else {
+                showToast('toast-error', 'Failed to update queue.');
+                console.error(response.status, 'Failed to update queue.');
+            }
+        } catch (error) {
+            showToast('toast-error', 'An error occurred while processing the request.');
+            console.error(response.status, error);
+            return null;
+        }
+    }
+</script>
