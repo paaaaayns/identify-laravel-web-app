57d3f305aef7b84acd69fa9dea274cbc93c1452e
paaaaayns <ejpines21@gmail.com>
2025-01-27 15:54:16 +0800

    feat: add biometrics storage configuration to filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -56,10 +56,15 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
-        
+
         'medical_records' => [
             'driver' => 'local',
             'root' => storage_path('app/medical-records'),
+        ],
+
+        'biometrics' => [
+            'driver' => 'local',
+            'root' => storage_path('app/biometrics'),
         ],
 
     ],
