1fc1cdc707ee345e51e142e6c0985b6c08ce1a81
paaaaayns <ejpines21@gmail.com>
2025-02-27 14:54:51 +0800

    feat: update page titles to 'iDentify' for login and pre-registration layouts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>iDentify</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
