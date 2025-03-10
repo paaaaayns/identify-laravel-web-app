953cd00a44d04685aedb8d9c4a9b7e50ea64e8d2
paaaaayns <ejpines21@gmail.com>
2025-03-10 13:35:45 +0800

    refactor: comment out unused seeder calls and improve folder deletion logging

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,28 +18,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Delete all files in public/patients
-        $this->deletePatientsFolder();
+        // $this->deletePatientsFolder();
 
         // Call the RoleSeeder
         $this->call(RoleSeeder::class);
-        // Call the AdminSeeder
         $this->call(AdminSeeder::class);
-        // Call the OpdSeeder
         $this->call(OpdSeeder::class);
-        // Call the DoctorSeeder
         $this->call(DoctorSeeder::class);
 
-
-        // // Seed pre-registered patient records
-        PreRegisteredPatient::factory(5)->create();
-        Opd::factory(5)->create();
-        Doctor::factory(5)->create();
-
-        // // Seed patient queue records
-        // PatientQueue::factory(5)->create();
-
-        // Seed user records
-        // php artisan migrate:rollback && php artisan migrate:fresh --seed
+        // Seed pre-registered patient records
+        // PreRegisteredPatient::factory(5)->create();
+        // Opd::factory(5)->create();
+        // Doctor::factory(5)->create();
     }
 
     /**
@@ -50,8 +40,7 @@ class DatabaseSeeder extends Seeder
         $folderPath = public_path('storage/patients');
 
         if (File::exists($folderPath)) {
-            File::cleanDirectory($folderPath); // Deletes all files but keeps the folder
-            // File::deleteDirectory($folderPath); // Use this instead if you want to remove the folder entirely
+            File::cleanDirectory($folderPath);
             Log::info('DatabaseSeeder@deletePatientsFolder: All files in the patients folder have been deleted.');
         } else {
             Log::info('DatabaseSeeder@deletePatientsFolder: The patients folder does not exist.');
