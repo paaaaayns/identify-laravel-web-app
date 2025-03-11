0a7267bfb9966c9f7e636cbe5945ff4e22bd894d
paaaaayns <ejpines21@gmail.com>
2025-03-11 09:22:17 +0800

    feat: enhance card-counter component with view and create links for better navigation

--- resources/views/auth/dashboard/admin.blade.php
+++ resources/views/auth/dashboard/admin.blade.php
@@ -1,25 +1,47 @@
 <x-layout>
     <div>
         <dl class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-2">
-            <x-card-counter href="{{ route('users.pre-reg.index') }}" :title="'Pre-Registered Patients'" :totalCount="$preRegPatientsCount" :recentCount="$recentPreRegPatientsCount">
+            <x-card-counter
+                viewLink="{{ route('users.pre-reg.index') }}"
+                createLink="{{ route('users.pre-reg.create') }}"
+                :title="'Pre-Registered Patients'"
+                :totalCount="$preRegPatientsCount"
+                :recentCount="$recentPreRegPatientsCount">
                 <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                 </svg>
             </x-card-counter>
 
-            <x-card-counter href="{{ route('users.patient.index') }}" :title="'Registered Patients'" :totalCount="$patientsCount" :recentCount="$recentPatientsCount">
+            <x-card-counter
+                :hasCreate="false"
+                viewLink="{{ route('users.patient.index') }}"
+                :title="'Registered Patients'"
+                :totalCount="$patientsCount"
+                :recentCount="$recentPatientsCount">
                 <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                 </svg>
             </x-card-counter>
 
-            <x-card-counter href="{{ route('users.opd.index') }}" :title="'OPDs'" :totalCount="$opdsCount" :recentCount="$recentOpdsCount">
+            <x-card-counter
+                viewLink="{{ route('users.opd.index') }}"
+                createLink="{{ route('users.opd.create') }}"
+                :title="'OPDs'"
+                :totalCount="$opdsCount"
+                :recentCount="$recentOpdsCount">
                 <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                 </svg>
             </x-card-counter>
 
-            <x-card-counter href="{{ route('users.doctor.index') }}" :title="'Doctors'" :totalCount="$doctorsCount" :recentCount="$recentDoctorsCount">
+            <x-card-counter
+                :hasCreate="true"
+                :hasView="true"
+                viewLink="{{ route('users.doctor.index') }}"
+                createLink="{{ route('users.doctor.create') }}"
+                :title="'Doctors'"
+                :totalCount="$doctorsCount"
+                :recentCount="$recentDoctorsCount">
                 <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                 </svg>
--- resources/views/components/card-counter.blade.php
+++ resources/views/components/card-counter.blade.php
@@ -2,7 +2,10 @@
 'title' => 'N/A',
 'totalCount' => '0',
 'recentCount' => '0',
-'href' => '#'
+'hasView' => true,
+'hasCreate' => true,
+'viewLink' => '#',
+'createLink' => '#'
 ])
 
 <div class="relative overflow-hidden rounded-lg bg-white px-4 pb-12 pt-5 shadow sm:px-6 sm:pt-6">
@@ -16,20 +19,28 @@
         <p class="text-2xl font-semibold text-gray-900"> {{ $totalCount }} </p>
 
         @if ($recentCount > 0)
-            <p class="ml-2 flex items-baseline text-sm font-semibold text-green-600">
-                <svg class="h-5 w-5 shrink-0 self-center text-green-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                    <path fill-rule="evenodd" d="M10 17a.75.75 0 0 1-.75-.75V5.612L5.29 9.77a.75.75 0 0 1-1.08-1.04l5.25-5.5a.75.75 0 0 1 1.08 0l5.25 5.5a.75.75 0 1 1-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0 1 10 17Z" clip-rule="evenodd" />
-                </svg>
-                <span class="sr-only"> Increased by </span>
-                {{ $recentCount }}
-            </p>
+        <p class="ml-2 flex items-baseline text-sm font-semibold text-green-600">
+            <svg class="h-5 w-5 shrink-0 self-center text-green-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                <path fill-rule="evenodd" d="M10 17a.75.75 0 0 1-.75-.75V5.612L5.29 9.77a.75.75 0 0 1-1.08-1.04l5.25-5.5a.75.75 0 0 1 1.08 0l5.25 5.5a.75.75 0 1 1-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0 1 10 17Z" clip-rule="evenodd" />
+            </svg>
+            <span class="sr-only"> Increased by </span>
+            {{ $recentCount }}
+        </p>
         @endif
 
         <div class="absolute inset-x-0 bottom-0 bg-secondary px-4 py-4 sm:px-6">
-            <div class="text-sm">
-                <a href="{{ $href }}" class="font-medium text-primary hover:text-primary">
+            <div class="text-sm flex space-x-4 ">
+                @if ($hasView)
+                <a href="{{ $viewLink }}" class="font-medium text-primary hover:text-primary">
                     View all
                 </a>
+                @endif
+
+                @if ($hasCreate)
+                <a href="{{ $createLink }}" class="font-medium text-primary hover:text-primary">
+                    Create
+                </a>
+                @endif
             </div>
         </div>
     </dd>
