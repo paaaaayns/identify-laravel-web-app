994a45055b9906b76bb008d26369fa7ab4ccbe49
paaaaayns <ejpines21@gmail.com>
2025-03-13 13:03:59 +0800

    feat: add profile update request classes with validation rules for admin, doctor, opd, and patient

---
+++ app/Http/Requests/UpdateOpdProfileRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateOpdProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'religion' => ['required', 'string', 'max:255'],
+            'civil_status' => ['required', 'string', 'max:255'],
+            'citizenship' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'contact_number' => ['required', 'regex:/^09[0-9]{7,13}$/', "min:11"],
+        ];
+    }
+
+    /**
+     * Custom error messages for validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'required' => 'This field is required',
+            'contact_number.regex' => 'Please enter a valid contact number',
+            'contact_number.min' => 'Please enter a valid contact number',
+            'max' => 'This field cannot be more than :max characters',
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdatePatientProfileRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePatientProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'religion' => ['required', 'string', 'max:255'],
+            'civil_status' => ['required', 'string', 'max:255'],
+            'citizenship' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'contact_number' => ['required', 'regex:/^09[0-9]{7,13}$/', "min:11"],
+
+            'emergency_contact1_name' => ['required', 'string', 'max:255'],
+            'emergency_contact1_number' => ['required', 'regex:/^09[0-9]{7,13}$/', "min:11"],
+            'emergency_contact1_relationship' => ['required', 'string', 'max:255'],
+            'emergency_contact2_name' => ['required', 'string', 'max:255'],
+            'emergency_contact2_number' => ['required', 'regex:/^09[0-9]{7,13}$/', "min:11"],
+            'emergency_contact2_relationship' => ['required', 'string', 'max:255'],
+
+        ];
+    }
+
+    /**
+     * Custom error messages for validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'required' => 'This field is required',
+            'contact_number.regex' => 'Please enter a valid contact number',
+            'contact_number.min' => 'Please enter a valid contact number',
+            'emergency_contact1_number.regex' => 'Please enter a valid contact number',
+            'emergency_contact1_number.min' => 'Please enter a valid contact number',
+            'emergency_contact2_number.regex' => 'Please enter a valid contact number',
+            'emergency_contact2_number.min' => 'Please enter a valid contact number',
+            'max' => 'This field cannot be more than :max characters',
+        ];
+    }
+}
