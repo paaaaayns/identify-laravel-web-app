c8c6234f1434dd2fbd28ca4becd95eb1b99753eb
paaaaayns <ejpines21@gmail.com>
2025-03-13 13:03:35 +0800

    feat: add profile picture SVG placeholder in doctor profile view

--- resources/views/auth/users/doctor/show.blade.php
+++ resources/views/auth/users/doctor/show.blade.php
@@ -44,6 +44,12 @@
     <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
         <!-- Left Column -->
         <div class="flex flex-col items-center text-center bg-white shadow rounded-lg md:col-span-1 p-6 self-start">
+            <!-- Profile Picture -->
+            <div class="w-40 h-40 mb-2">
+                <svg class="w-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="#1F555F" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+            </div>
             <!-- User Info -->
             <h2 class="text-lg font-semibold text-gray-800">{{ $profile->last_name }}, {{ $profile->first_name }} {{ $profile->middle_name ?? '' }}</h2>
             <p class="text-sm text-gray-500">{{ $profile->user_id }}</p>
